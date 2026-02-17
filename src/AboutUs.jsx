57a9fa397ee0f4123eb13e5d18bf486cab545903
jony <jonymoin39@gmail.com>
2026-02-17 19:43:40 +0600

    Initial commit

--- src/AboutUs.jsx
+++ src/AboutUs.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const AboutUs = () => {
   return (
-    <section className="bg-gray-50 py-12 px-6 md:px-16 lg:px-24">
+    <section className="bg-lime-600 py-12 px-6 md:px-16 lg:px-24">
       <div className="max-w-5xl mx-auto text-center">
         <h2 className="text-3xl md:text-4xl font-bold text-gray-800 mb-6">
           About Us
