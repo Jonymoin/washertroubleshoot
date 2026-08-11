dee351de9f2dbe19fb9030add4f8126f725aa2b1
jony <jonymoin39@gmail.com>
2026-08-11 21:40:11 +0600

    route

--- src/App.tsx
+++ src/App.tsx
@@ -53,9 +53,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
-        <WouterRouter
-          base={import.meta.env.BASE_URL.replace(/\/$/, "")}
-        >
+        <WouterRouter>
           <Layout>
             <Router />
           </Layout>
